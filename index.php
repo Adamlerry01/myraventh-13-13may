bdf947bcb4ced530fe75d9d0c206f855cc77ca4c
Adamlerry01 <firojwilliam9248@gmail.com>
2026-05-13 00:44:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
 		<!-- Header -->
 			<header id="header" class="alt"><h1><a href="index.html">Retrospect</a></h1>
 				<a href="#nav">Menu</a>
-			</header><!-- Nav --><nav id="nav"><ul class="links"><li><a href="index.html">Home</a></li>
+			</header>
+	<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5026336,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5026336&101" alt="free geoip" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+	<!-- Nav --><nav id="nav"><ul class="links"><li><a href="index.html">Home</a></li>
 					<li><a href="generic.html">Generic</a></li>
 					<li><a href="elements.html">Elements</a></li>
 				</ul></nav><!-- Banner --><section id="banner"><i class="icon fa-diamond"></i>
